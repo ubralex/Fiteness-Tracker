bb18495a603e6f9663671594f744d5a5a45e2ae1
Alex Brito <alexanderfloresbrito@gmail.com>
2023-02-09 18:43:39 -0300

    Fitness Tracker

---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -0,0 +1,24 @@
+package co.tiagoaguiar.fitnesstracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            // navegar para a próxima tela
+            val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+        }
+    }
+}

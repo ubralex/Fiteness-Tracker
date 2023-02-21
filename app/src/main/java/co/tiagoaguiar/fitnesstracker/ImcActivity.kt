172b0ba742de4f928a1a3314776ae75991ed48a9
Alex Brito <alexanderfloresbrito@gmail.com>
2023-02-21 20:17:01 -0300

    Fitness Tracker

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -2,10 +2,51 @@ package co.tiagoaguiar.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+
+            val result = calculateImc(weight, height)
+            Log.d("Teste", "resultado: $result")
+
+        }
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        // PESO / (ALTURA * ALTURA)
+        return weight / ((height / 100.0) * (height / 100.0))
+    }
+
+    private fun validate(): Boolean {
+        // NÃO PODE INSERIR VALORES NULOS (NULL)
+        // NÃO PODE COMEÇAR COM 0 (ZERO)
+
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
     }
 }

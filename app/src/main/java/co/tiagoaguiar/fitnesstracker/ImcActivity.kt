a3eff895920e7b13c3cb00fa64aa33d90cd69d70
Alex Brito <alexanderfloresbrito@gmail.com>
2023-03-03 16:13:36 -0300

    Fitness Tracker

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,9 +1,11 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -33,7 +35,7 @@ class ImcActivity : AppCompatActivity() {
             val height = editHeight.text.toString().toInt()
             val result = calculateImc(weight, height)
             val imcResponseId = imcResponse(result)
-            val dialog = AlertDialog.Builder(this)
+            AlertDialog.Builder(this)
                 .setTitle(getString(R.string.imc_response, result))
                 .setMessage(imcResponseId)
                 .setPositiveButton(android.R.string.ok) { dialog, which ->
@@ -41,7 +43,8 @@ class ImcActivity : AppCompatActivity() {
                 }
                 .create()
                 .show()
-
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 

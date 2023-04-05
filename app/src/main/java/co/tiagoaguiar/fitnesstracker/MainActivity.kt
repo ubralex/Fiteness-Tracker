a2b9970f347cf562a1ffb898569cb131b05a7e2d
Alex Brito <alexanderfloresbrito@gmail.com>
2023-04-05 14:37:20 -0300

    Fitness Tracker

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,9 +1,15 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,37 +21,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainAdapter()
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.baseline_wb_sunny_24,
+                textStringId = R.string.label_imc,
+                color = Color.GREEN
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.baseline_visibility_24,
+                textStringId = R.string.label_tmb,
+                color = Color.YELLOW
+            )
+        )
+
+        val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
-        rvMain.layoutManager = LinearLayoutManager(this)
+        rvMain.layoutManager = GridLayoutManager(this, 2)
 
-//        btnImc = findViewById(R.id.btn_imc)
-//
-//        btnImc.setOnClickListener {
-//            // navegar para a próxima tela
-//            val i = Intent(this, ImcActivity::class.java)
-//            startActivity(i)
-//        }
     }
 
-    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(private val mainItems: List<MainItem>) :
+        RecyclerView.Adapter<MainViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             return MainViewHolder(view)
         }
 
         override fun getItemCount(): Int {
-            return 30
+            return mainItems.size
         }
 
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
         }
 
     }
 
-    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem) {
+            val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+            val name: TextView = itemView.findViewById(R.id.item_txt_name)
+            val container: LinearLayout = itemView.findViewById(R.id.item_container_imc)
 
+            img.setImageResource(item.drawableId)
+            name.setText(item.textStringId)
+            container.setBackgroundColor(item.color)
+        }
     }
 }
